09fd0a395b53fc75d26fea4046495095f14325ab
Muhammad Asri Nasution <nasution.nam@gmail.com>
2019-12-01 07:10:28 +0700

    Stateless & Statefull Component in React

--- src/index.js
+++ src/index.js
@@ -4,7 +4,27 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Stateless Component
+const Hello = () => {
+    return (
+        <p>Hello</p>
+    )
+}
+
+// StateFul Component
+
+class StateFulComponent extends React.Component
+{
+    render() 
+    {
+        return (
+            <p>StateFul Component</p>
+        )
+        
+    }
+}
+
+ReactDOM.render(<StateFulComponent />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
